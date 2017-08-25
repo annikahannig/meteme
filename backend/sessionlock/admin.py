c3d1a0d0867f022741f7c35bbf42b580acd899a9
Matthias Hannig <matthias@hannig.cc>
2017-08-25 13:58:37 +0200

    add locking / unlocking and session tracking

--- backend/sessionlock/admin.py
+++ backend/sessionlock/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from sessionlock import models
 
-# Register your models here.
+class LockAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'session_active',
+        'client_identifier',
+        'client_ip',
+        'user_agent',
+    ]
+
+    list_display_links = ['user']
+
+    readonly_fields = [
+        'user',
+        'client_ip',
+        'user_agent',
+        'client_identifier',
+    ]
+
+
+admin.site.register(models.Lock, LockAdmin)
+
+
--- backend/sessionlock/models.py
+++ backend/sessionlock/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.sessions.models import Session as DjangoSession
+from django.contrib.auth.models import User as AuthUser
 
 
 class Lock(models.Model):
@@ -14,6 +15,27 @@ class Lock(models.Model):
                                 blank=False,
                                 on_delete=models.CASCADE)
 
+
+    # Some metadata
+    user_agent = models.CharField(max_length=255)
+
+    client_ip = models.CharField(max_length=40)
+    client_identifier = models.CharField(max_length=60)
+
+    # The actual switch
     session_active = models.BooleanField(default=False)
+
+    # Keep track of this
     updated_at = models.DateTimeField(auto_now=True)
+
+
+    @property
+    def user(self):
+        """Get the corresponding auth user"""
+        auth_user_id = self.session.get_decoded().get('_auth_user_id')
+        if not auth_user_id:
+            return None
+        user = AuthUser.objects.get(pk=auth_user_id)
+        return user
+
 
